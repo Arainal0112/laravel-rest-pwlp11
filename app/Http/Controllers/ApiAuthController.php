4b44e509662def32729d58b6a93216b23cf81b0c
Arainal0112 <arainalaldi0112@gmail.com>
2023-05-30 15:49:46 +0700

    Jobsheet P13 : RESTful API 2 - Praktikum 3

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -6,8 +6,9 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use App\Http\Requests\LoginRequest;
-use App\Http\Resources\LoginResource;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Resources\LoginResource;
+use App\Http\Requests\RegisterRequest;
 
 class ApiAuthController extends Controller
 {
@@ -29,12 +30,26 @@ class ApiAuthController extends Controller
             'token' => $token,
         ],200);
     }
-    
+
     public function logout(Request $request){
         #Hapus
         $request->user()->tokens()->delete();
 
         #respon
         return response()->noContent();
+    }
+    public function register(RegisterRequest $request){
+        $user=User::create([
+            'username'=>$request->username,
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password)
+        ]);
+        $token = $user->createToken('token')->plainTextToken;
+        return new LoginResource([
+            'message'=>'succes login',
+            'user' => $user,
+            'token' => $token,
+        ],200);
     }
 }
